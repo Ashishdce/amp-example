72b3e12436550744ca0362899a486d8053445ffc
CIW <CIW@BLRASIVAS09691.sapient.com>
2017-06-04 15:50:21 +0530

    port corrected

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
 // listen (start app with node index.js) 
-app.listen(8080);
-console.log("App listening on port 8080");
+app.listen(app.get('port'),function(){
+    console.log("App listening on port ", app.get('port'));
+});
